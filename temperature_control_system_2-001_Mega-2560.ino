c5567b4d907fb4a78ea024d8af009a164e8e363c
Hanna <hanna.paivasto@gmail.com>
2025-06-25 18:42:57 +0300

    code_pre

--- temperature_control_system_2-001_Mega-2560.ino
+++ temperature_control_system_2-001_Mega-2560.ino
@@ -1,6 +1,17 @@
 // temperature_control_system_2-001_Mega-2560
 // en Hanna P 2025
 
+/********************************************/
+
+/*-----( Import needed libraries )-----*/
+#include <Wire.h>  // Comes with Arduino IDE
+
+// Get the LCD I2C Library here:
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+// Move any other LCD libraries to another folder or delete them
+// See Library "Docs" folder for possible commands etc.
+#include <LiquidCrystal_I2C.h>
+
 void setup() {
   // put your setup code here, to run once:
 
