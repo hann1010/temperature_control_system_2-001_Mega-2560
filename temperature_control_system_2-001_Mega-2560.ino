af63dc54d68b4749599325cdc38b85ec0ceab976
Hanna <hanna.paivasto@gmail.com>
2026-02-12 23:28:01 +0200

    update

--- temperature_control_system_2-001_Mega-2560.ino
+++ temperature_control_system_2-001_Mega-2560.ino
@@ -68,7 +68,6 @@ Note for SoftwareSerial:
  10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
 
  Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
  
 -------------------------------------------------------------------*/
 
